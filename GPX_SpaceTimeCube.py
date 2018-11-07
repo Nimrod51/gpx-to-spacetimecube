fb9837dd479f01cf890e8e16bb4f6ce6cd0dec4d
Nimrod51 <gavishnimrod@gmail.com>
2018-11-07 08:32:46 +0100

    fix path input

--- GPX_SpaceTimeCube.py
+++ GPX_SpaceTimeCube.py
@@ -31,7 +31,6 @@ import os.path
 
 class GPXtoSpaceTimeCube:
     """QGIS Plugin Implementation."""
-    gpx_path=""
     def __init__(self, iface):
         """Constructor.
 
@@ -184,108 +183,107 @@ class GPXtoSpaceTimeCube:
 
     def getGPXPath(self):
         from PyQt4.QtGui import QAction, QIcon, QFileDialog
-        self.gpx_path = QFileDialog.getOpenFileName(self.dlg, "select","",'*.gpx')
-        self.dlg.lineEdit.setText(self.gpx_path)
-
+        gpx_path = QFileDialog.getOpenFileName(self.dlg, "select","",'*.gpx')
+        if os.path.exists(gpx_path):
+            self.dlg.lineEdit.setText(gpx_path)
+        else:
+            self.dlg.lineEdit.setText("")
+        
     def plotGPX(self):
         import ogr, os
         import time, datetime
         import matplotlib as mpl
         from mpl_toolkits.mplot3d import Axes3D
-        #import numpy as np
         import matplotlib.pyplot as plt
 
-        if os.path.exists(self.gpx_path):
-            gpx_path=self.gpx_path
-        else:
-            print "GPX path is not selected"
+        gpx_path = self.dlg.lineEdit.text()
+        if gpx_path == "":
             self.dlg.status.setText("No file selected")
             self.dlg.status.setStyleSheet('background-color: red;')
-            return
-        
-        #Open GPX
-        in_path = os.path.join (gpx_path)
-        in_driver= ogr.GetDriverByName("GPX")
-        data_source = in_driver.Open(in_path,0)
+        else:            
+            #Open GPX
+            in_path = os.path.join (gpx_path)
+            in_driver= ogr.GetDriverByName("GPX")
+            data_source = in_driver.Open(in_path,0)
 
-        #Plot parameters
-        mpl.rcParams ['legend.fontsize'] = 10
-        fig = plt.figure()
-        ax = fig.gca(projection='3d')
+            #Plot parameters
+            mpl.rcParams ['legend.fontsize'] = 10
+            fig = plt.figure()
+            ax = fig.gca(projection='3d')
 
-        #Create layer from data source
-        if data_source is None:
-            #print "Could not open %s" % (in_path)
-            self.dlg.status.setText("Data source is invalid")
-            self.dlg.status.setStyleSheet('background-color: red;')
-        else:
-            #print "Opened %s" % (in_path)
-            layer = data_source.GetLayer(4) # 0 - waypoints, 1 - routes, 2 - tracks, 3 - route points, 4 - track points 
-                    
-        #Get time and spatial data from GPX dynamically and plot
-        x=[] #Latitude
-        y=[] #Latitude
-        z=[] ##Time in minutes since start 
-        elapsedTime=[] #Time in seconds since epoch
-        DTArray=[] ##Array of python strings
-        DateTimeArray=[] #Python datetime object of each point
-        schema=[] #All field names
+            #Create layer from data source
+            if data_source is None:
+                #print "Could not open %s" % (in_path)
+                self.dlg.status.setText("Data source is invalid")
+                self.dlg.status.setStyleSheet('background-color: red;')
+            else:
+                #print "Opened %s" % (in_path)
+                layer = data_source.GetLayer(4) # 0 - waypoints, 1 - routes, 2 - tracks, 3 - route points, 4 - track points 
+                        
+            #Get time and spatial data from GPX dynamically and plot
+            x=[] #Latitude
+            y=[] #Latitude
+            z=[] ##Time in minutes since start 
+            elapsedTime=[] #Time in seconds since epoch
+            DTArray=[] ##Array of python strings
+            DateTimeArray=[] #Python datetime object of each point
+            schema=[] #All field names
 
-        ldefn = layer.GetLayerDefn()
-        for n in range(ldefn.GetFieldCount()):
-            fdefn = ldefn.GetFieldDefn(n)
-            schema.append(fdefn.name)
-        try:
-            timeIndex=schema.index('time')
-        except ValueError:
-            #print "No time field found"
-            self.dlg.status.setText("No time field found")
-            self.dlg.status.setStyleSheet('background-color: red;')
+            ldefn = layer.GetLayerDefn()
+            for n in range(ldefn.GetFieldCount()):
+                fdefn = ldefn.GetFieldDefn(n)
+                schema.append(fdefn.name)
+            try:
+                timeIndex=schema.index('time')
+            except ValueError:
+                #print "No time field found"
+                self.dlg.status.setText("No time field found")
+                self.dlg.status.setStyleSheet('background-color: red;')
 
-        try:
-            for feat in layer:
-                pt = feat.geometry()
-                x.append(pt.GetX())
-                y.append(pt.GetY())
-                dateTime=feat.GetField(timeIndex)
-                try:
-                    DT=datetime.datetime.strptime(dateTime, "%Y/%m/%d %H:%M:%S+00" )
-                except ValueError:
-                    DT=datetime.datetime.strptime(dateTime, "%Y/%m/%d %H:%M:%S.%f+00" )
-                DateTimeArray.append(DT)
-                SSE= time.mktime(DT.timetuple()) #Seconds since epoch
-                elapsedTime.append(SSE)
-                DTArray.append(DT.strftime("%H:%M:%S"))
-        except TypeError:
-            print "Time field does contain none or invalid dates"
-            self.dlg.status.setText("Time field does contain none or invalid dates")
-            self.dlg.status.setStyleSheet('background-color: red;')
+            try:
+                for feat in layer:
+                    pt = feat.geometry()
+                    x.append(pt.GetX())
+                    y.append(pt.GetY())
+                    dateTime=feat.GetField(timeIndex)
+                    try:
+                        DT=datetime.datetime.strptime(dateTime, "%Y/%m/%d %H:%M:%S+00" )
+                    except ValueError:
+                        DT=datetime.datetime.strptime(dateTime, "%Y/%m/%d %H:%M:%S.%f+00" )
+                    DateTimeArray.append(DT)
+                    SSE= time.mktime(DT.timetuple()) #Seconds since epoch
+                    elapsedTime.append(SSE)
+                    DTArray.append(DT.strftime("%H:%M:%S"))
+            except TypeError:
+                print "Time field does contain none or invalid dates"
+                self.dlg.status.setText("Time field does contain none or invalid dates")
+                self.dlg.status.setStyleSheet('background-color: red;')
 
-        #Create sub title for plot
-        title=DateTimeArray[0].strftime("%Y/%m/%d")+" " +DTArray[0]+" to " + DateTimeArray[-1].strftime("%Y/%m/%d")+" "+DTArray[-1]
+            #Create sub title for plot
+            title=DateTimeArray[0].strftime("%Y/%m/%d")+" " +DTArray[0]+" to " + DateTimeArray[-1].strftime("%Y/%m/%d")+" "+DTArray[-1]
 
-        #Extract elapsed time in minutes 
-        z.append(0) #First item in elapsedTime array should be 0 
-        for i in range(1,len(elapsedTime)):
-            z.append((elapsedTime[i]-elapsedTime[0])/60)
+            #Extract elapsed time in minutes 
+            z.append(0) #First item in elapsedTime array should be 0 
+            for i in range(1,len(elapsedTime)):
+                z.append((elapsedTime[i]-elapsedTime[0])/60)
 
-        #Plot X,Y,Z
-        ax.plot(x,y,z)
+            #Plot X,Y,Z
+            ax.plot(x,y,z)
 
-        #Labels & Plot
-        ax.set_xlabel('Longitude')
-        ax.set_ylabel('Latitude')
-        ax.set_zlabel('Time (Minutes)')
-        ax.legend()
-        plt.axis("equal")
-        fig.suptitle('Space Time Cube', fontsize=12, fontweight='bold')
-        plt.title(title,loc='center')
-        plt.show()
-        #Optionally save the image
-        #plt.savefig("C:/Output/SpaceTime.jpg", dpi=100, format="jpg")
-        #print "SUCCESS"
-        self.dlg.status.setText("SUCCESS")
-        self.dlg.status.setStyleSheet('background-color: green;')
+            #Labels & Plot
+            ax.set_xlabel('Longitude')
+            ax.set_ylabel('Latitude')
+            ax.set_zlabel('Time (Minutes)')
+            ax.legend()
+            plt.axis("equal")
+            fig.suptitle('Space Time Cube', fontsize=12, fontweight='bold')
+            plt.title(title,loc='center')
+            plt.show()
+            #Optionally save the image
+            #plt.savefig("C:/Output/SpaceTime.jpg", dpi=100, format="jpg")
+            #print "SUCCESS"
+            self.dlg.status.setText("SUCCESS")
+            self.dlg.status.setStyleSheet('background-color: green;')
                  
 
     def run(self):
